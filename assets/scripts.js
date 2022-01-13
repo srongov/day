52d7d86117f99968e3538997d0bfe21962101454
Hak Gov <83107437+srongov@users.noreply.github.com>
2022-01-13 16:01:10 -0500

    Add files via upload

---
+++ assets/scripts.js
@@ -0,0 +1,62 @@
+var saveBtn = $(".saveBtn");
+
+/**
+ * FUNCTIONS
+ */
+
+// current day is displayed at the top of the calendar
+$("#currentDay").text(moment(new Date()).format("DD/MM/YYYY"));
+
+// each time block is color-coded to indicate whether it is in the past, present, or future
+function timeBlockColor() {
+    var hour = moment().hours();
+
+    $(".time-block").each(function() {
+        var currHour = parseInt($(this).attr("id"));
+
+        // console.log(this); //each time-block
+
+        if (currHour > hour) {
+            $(this).addClass("future");
+        } else if (currHour === hour) {
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("past");
+        }
+    })
+};
+
+// WHEN I click the save button for that time block
+saveBtn.on("click", function() {
+
+    // console.log(this); //save button
+    var time = $(this).siblings(".hour").text();
+    var plan = $(this).siblings(".plan").val();
+
+    // THEN the text for that event is saved in local storage
+    localStorage.setItem(time, plan);
+});
+
+// WHEN I refresh the page
+// THEN the saved events persist
+function usePlanner() {
+
+    $(".hour").each(function() {
+        var currHour = $(this).text();
+        var currPlan = localStorage.getItem(currHour);
+
+        // console.log(this);
+        // console.log(currHour);
+
+        if(currPlan !== null) {
+            $(this).siblings(".plan").val(currPlan);
+        }
+    });
+}
+
+/**
+ * CALL FUNCTIONS
+ */
+
+timeBlockColor();
+usePlanner();
